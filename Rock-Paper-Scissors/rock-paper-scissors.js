5b7fd20a800e82747e44536388449e6c6acb169d
Tossy06 <davidgomez.sp06@gmail.com>
2024-11-20 17:45:31 -0500

    Page-V1

--- Rock-Paper-Scissors/rock-paper-scissors.js
+++ Rock-Paper-Scissors/rock-paper-scissors.js
@@ -1,1 +1,13 @@
-console.log('Holaa');
+document.addEventListener("DOMContentLoaded", () => {
+    // Mostrar el cuerpo de la página cuando el DOM está listo
+    document.body.style.display = "block";
+    var rock = document.getElementById('rock');
+    var paper = document.getElementById('paper');
+    var scissors = document.getElementById('scissors');
+    var player_score = document.getElementById('score-player')
+    var machine_score = document.getElementById('machine-selection');
+    const buttons_attack = document.querySelectorAll('.buttons-attack');
+    
+
+    
+});
